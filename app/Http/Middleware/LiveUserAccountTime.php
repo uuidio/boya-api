919c04b9a5d2c69fdf63c5459244e19b5f6b5591
li <siuisune@163.com>
2021-02-19 10:31:15 +0800

    fix

--- app/Http/Middleware/LiveUserAccountTime.php
+++ app/Http/Middleware/LiveUserAccountTime.php
@@ -3,20 +3,30 @@
 namespace ShopEM\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Auth;
+use ShopEM\Models\LiveUsers;
 
 class LiveUserAccountTime
 {
     /**
      * Handle an incoming request.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
+        $user_id = Auth::guard('live_users')->user()->id;
+        $user = LiveUsers::where('id', $user_id)->select('id', 'account_end_time')->first();
+        \Log::info('$user_id:' . $user_id);
 
-        \Log::info('LiveUserAccountTime');
+        if ($user) {
+            if ($user->account_end_time < time()) {
+                return response("您的账号已过期，请续费", 406);
+            }
+        }
+
 
         return $next($request);
     }
--- routes/Live/V1.php
+++ routes/Live/V1.php
@@ -29,73 +29,79 @@ Route::namespace('ShopEM\Http\Controllers\Live\V1')->group(function () {
     Route::get('versions/check', 'PassportController@versions');    //版本
 });
 
-Route::namespace('ShopEM\Http\Controllers\Live\V1')->middleware('auth:live_users', 'live.user.account')->group(function () {
+Route::namespace('ShopEM\Http\Controllers\Live\V1')->middleware('auth:live_users')->group(function () {
 
-    Route::get('live/oauth', 'PassportController@loginOauth');   //
-    Route::post('live/begin', 'LiveController@begin');   // 开始直播
-    Route::get('live/end', 'LiveController@end');   // 关闭直播
-    Route::get('live/record', 'LiveController@liveRecord');   // 开始录制
-    Route::get('live/playback', 'LiveController@playback');   // 回放
-    Route::post('live/playback/delete', 'LiveController@delPlayback');   // 回放删除
+    //主播账号有效期限
+    Route::middleware('live.user.account')->group(function () {
 
-    Route::get('live/audience', 'LiveController@audience');   // 关闭直播
+        Route::get('live/oauth', 'PassportController@loginOauth');   //
+        Route::post('live/begin', 'LiveController@begin');   // 开始直播
+        Route::get('live/end', 'LiveController@end');   // 关闭直播
+        Route::get('live/record', 'LiveController@liveRecord');   // 开始录制
+        Route::get('live/playback', 'LiveController@playback');   // 回放
+        Route::post('live/playback/delete', 'LiveController@delPlayback');   // 回放删除
 
-    Route::get('live/close', 'LiveController@closeLive');   // 强制关闭直播
+        Route::get('live/audience', 'LiveController@audience');   // 关闭直播
 
-    /*
-     * 主播商品
-     */
-    Route::get('goods/list', 'LiveController@goods');    // 主播端商品列表
-    Route::post('goods/save', 'LiveController@saveGoods');    // 主播端商品保存
-    Route::post('goods/delete', 'LiveController@delGoods');    // 主播端商品删除
-    Route::post('goods/showSave', 'LiveController@showUpdateGoods');    //商品悬浮更新
-    Route::get('goods/show', 'LiveController@showGoods');    //商品悬浮显示
+        Route::get('live/close', 'LiveController@closeLive');   // 强制关闭直播
 
-    /*
-     * 主播会员
-     */
-    Route::get('anchor/logout', 'PassportController@logout');    // 主播端退出登录
-    Route::post('anchor/modify', 'PassportController@modifyUser');    // 主播端信息更新
-    Route::get('anchor/detail', 'PassportController@detail');    // 主播端信息
+        /*
+         * 主播商品
+         */
+        Route::get('goods/list', 'LiveController@goods');    // 主播端商品列表
+        Route::post('goods/save', 'LiveController@saveGoods');    // 主播端商品保存
+        Route::post('goods/delete', 'LiveController@delGoods');    // 主播端商品删除
+        Route::post('goods/showSave', 'LiveController@showUpdateGoods');    //商品悬浮更新
+        Route::get('goods/show', 'LiveController@showGoods');    //商品悬浮显示
 
-    Route::get('assistan/get', 'LiveController@assistan');    // 主播端信息
+        /*
+         * 主播会员
+         */
+        Route::get('anchor/logout', 'PassportController@logout');    // 主播端退出登录
+        Route::post('anchor/modify', 'PassportController@modifyUser');    // 主播端信息更新
+        Route::get('anchor/detail', 'PassportController@detail');    // 主播端信息
 
-    /*
-     * 直播预告
-     */
-    Route::post('foreshow/add', 'ForeshowController@add');    //预告添加
-    Route::get('foreshow/list', 'ForeshowController@list');    //预告列表
-    Route::post('foreshow/delete', 'ForeshowController@delete');    //预告删除
-    Route::post('foreshow/update', 'ForeshowController@update');    //预告更新
-    Route::get('foreshow/edit', 'ForeshowController@edit');    //预告详情
+        Route::get('assistan/get', 'LiveController@assistan');    // 主播端信息
+
+        /*
+         * 直播预告
+         */
+        Route::post('foreshow/add', 'ForeshowController@add');    //预告添加
+        Route::get('foreshow/list', 'ForeshowController@list');    //预告列表
+        Route::post('foreshow/delete', 'ForeshowController@delete');    //预告删除
+        Route::post('foreshow/update', 'ForeshowController@update');    //预告更新
+        Route::get('foreshow/edit', 'ForeshowController@edit');    //预告详情
 
 
-    /*
-     * 设备端
-     */
-    Route::post('autocue/classify/add', 'EquipmentController@autocueClassifyAdd');    //提词器分类添加
-    Route::get('autocue/classify/list', 'EquipmentController@autocueClassifyList');    //提词器分类列表
-    Route::post('autocue/classify/delete', 'EquipmentController@autocueClassifyDel');    //提词器分类删除
-    Route::post('autocue/classify/save', 'EquipmentController@autocueClassifySave');    //提词器分类编辑
+        /*
+         * 设备端
+         */
+        Route::post('autocue/classify/add', 'EquipmentController@autocueClassifyAdd');    //提词器分类添加
+        Route::get('autocue/classify/list', 'EquipmentController@autocueClassifyList');    //提词器分类列表
+        Route::post('autocue/classify/delete', 'EquipmentController@autocueClassifyDel');    //提词器分类删除
+        Route::post('autocue/classify/save', 'EquipmentController@autocueClassifySave');    //提词器分类编辑
 
-    Route::post('autocue/add', 'EquipmentController@autocueAdd');    //提词器添加
-    Route::get('autocue/list', 'EquipmentController@autocueList');    //提词器列表
-    Route::post('autocue/delete', 'EquipmentController@autocueDel');    //提词器删除
-    Route::post('autocue/save', 'EquipmentController@autocueSave');    //提词器编辑
+        Route::post('autocue/add', 'EquipmentController@autocueAdd');    //提词器添加
+        Route::get('autocue/list', 'EquipmentController@autocueList');    //提词器列表
+        Route::post('autocue/delete', 'EquipmentController@autocueDel');    //提词器删除
+        Route::post('autocue/save', 'EquipmentController@autocueSave');    //提词器编辑
 
-    Route::post('tag/add', 'EquipmentController@tagsAdd');    //素材分类添加
-    Route::get('tag/list', 'EquipmentController@tagsList');    //素材分类列表
-    Route::post('tag/delete', 'EquipmentController@tagsDel');    //素材分类删除
-    Route::post('tag/save', 'EquipmentController@tagSave');    //素材分类编辑
+        Route::post('tag/add', 'EquipmentController@tagsAdd');    //素材分类添加
+        Route::get('tag/list', 'EquipmentController@tagsList');    //素材分类列表
+        Route::post('tag/delete', 'EquipmentController@tagsDel');    //素材分类删除
+        Route::post('tag/save', 'EquipmentController@tagSave');    //素材分类编辑
 
-    Route::post('tag/image/add', 'EquipmentController@tagsImageAdd');    //素材分类图片添加
-    Route::post('tag/image/delete', 'EquipmentController@tagsImageDel');    //素材分类图片删除
-    Route::get('tag/image/list', 'EquipmentController@tagsImageList'); //素材分类图片列表
+        Route::post('tag/image/add', 'EquipmentController@tagsImageAdd');    //素材分类图片添加
+        Route::post('tag/image/delete', 'EquipmentController@tagsImageDel');    //素材分类图片删除
+        Route::get('tag/image/list', 'EquipmentController@tagsImageList'); //素材分类图片列表
 
-    Route::get('liveTag/image/list', 'EquipmentController@liveTagsImageList'); //素材分类图片列表
+        Route::get('liveTag/image/list', 'EquipmentController@liveTagsImageList'); //素材分类图片列表
 
-    Route::post('tagImageApp/save', 'EquipmentController@tagsImageStatusSave');//app报错选中图片
-    Route::get('notice/get', 'EquipmentController@notice');//app报错选中图片
+        Route::post('tagImageApp/save', 'EquipmentController@tagsImageStatusSave');//app报错选中图片
+        Route::get('notice/get', 'EquipmentController@notice');//app报错选中图片
+
+
+    });
 });
 Route::namespace('ShopEM\Http\Controllers\Live\V1')->middleware('auth:assistant_users')->group(function () {
 
